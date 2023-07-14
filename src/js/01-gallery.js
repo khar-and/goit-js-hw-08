0048adc31d5e3329857af78b5dba9dbb41f55bc7
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-07-14 21:37:33 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const ulElem = document.querySelector('.gallery');
+const markupGalleryItems = createMarkupGallery(galleryItems);
+ulElem.insertAdjacentHTML('beforeend', markupGalleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a',{captionsData: "alt", captionDelay: 250});
+
+
+function createMarkupGallery(galleryItems) {
+    const markup = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;  
+              
+    });
+    return (markup.join(''));
+}
